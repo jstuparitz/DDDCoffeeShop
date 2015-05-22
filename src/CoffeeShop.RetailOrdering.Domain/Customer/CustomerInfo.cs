a59eeecc40d136ab5ab393f11be5c23dc4ec40fd
laurengawron <lochalatech@gmail.com>
2015-05-21 22:08:01 -0500

    Lauren, added customer class
    customer class config

---
+++ src/CoffeeShop.RetailOrdering.Domain/Customer/CustomerInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.RetailOrdering.Domain.Customer
+{
+    class CustomerInfo
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+
+           public Custo1mer(string firstName, string lastName, string email)
+           {
+                FirstName = firstName;
+                lastName = lastName;
+                Email = email;
+           }
+
+    }
+}
